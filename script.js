3c6054162bb48468c73899d67491e8b1a9a83ee7
jamunavenkatesan85-cloud <jamunavenkatesan85@gmail.com>
2025-09-01 18:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+  document.getElementById("myBtn").onclick = function(){
+
+   document.body.style.backgroundColor = "lightblue";
+
+   document.getElementById("showText").innerHTML = "You clicked the button!";
+
+}
